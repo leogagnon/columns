4fa8dee5979f9749a8a742f6700d07f519a9f91a
Leo Gagnon <leogagnon@gmail.com>
2022-09-01 16:52:42 -0400

    PL CLI integration

--- src/custom_transforms.py
+++ src/custom_transforms.py
@@ -2,59 +2,7 @@ from torchvision.transforms import Lambda, Compose, CenterCrop, RandAugment, Aut
     RandomInvert, RandomPosterize, RandomSolarize, RandomResizedCrop, RandomAffine, GaussianBlur, RandomHorizontalFlip, \
     Resize, RandomApply, ColorJitter, RandomGrayscale, RandomPerspective, RandomRotation, ToTensor, Normalize, \
     RandomErasing, CenterCrop
-from pl_bolts.transforms.dataset_normalizations import cifar10_normalization
-from datamodules import cifar100_normalization
-from utils import TwoCropTransform
 import torch
-
-
-class Transforms:
-    def __init__(self, image_size):
-        self.base_transforms = dict(
-            MNIST=Compose([
-                RandAugment(),
-                ToTensor(),
-                Normalize((0.5,), (0.5,)),
-            ]),
-            FashionMNIST=Compose([
-                RandAugment(),
-                ToTensor(),
-                Normalize((0.5,), (0.5,)),
-            ]),
-            smallNORB=Compose([
-                Resize((image_size, image_size)),
-                RandomCrop(32, padding=0),
-                RandAugment(),
-                ToTensor(),
-                Normalize((0.5,), (0.5,)),
-            ]),
-            CIFAR10=Compose([
-                Resize((image_size, image_size)),
-                RandAugment(),
-                ToTensor(),
-                cifar10_normalization(),
-            ]),
-            CIFAR100=Compose([
-                Resize((image_size, image_size)),
-                ToTensor(),
-                cifar100_normalization(),
-            ]),
-            ImageNet=Compose([
-                Resize(image_size + 32),
-                CenterCrop(image_size),
-                ToTensor(), Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225]
-                )]
-            )
-        )
-        self.corrupt_transforms = dict(
-            MNIST=Compose([
-                GaussianNoise(mean=0., std=0.1),
-                RandomPatchErasing(patch_size=4, p=0.3)
-            ])
-        )
-
 
 class RandomPatchErasing(torch.nn.Module):
 
